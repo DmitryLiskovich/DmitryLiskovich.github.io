0110532b9f036e010a7b83771a513838d4d27b86
Dmitry Liskovich <Dima_liskovich@mail.ru>
2019-06-29 20:16:05 +0300

    Updates

---
+++ precache-manifest.bf904757a5b1cc3c4d55ae11d398ecf8.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "04a7b5ae36cf0e3396261f4dfd981e06",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b21c6b8f0e3c946d7844",
+    "url": "/static/css/main.af33ec35.chunk.css"
+  },
+  {
+    "revision": "dd9c21cd30a259ab3788",
+    "url": "/static/js/2.4e098799.chunk.js"
+  },
+  {
+    "revision": "b21c6b8f0e3c946d7844",
+    "url": "/static/js/main.02548d0a.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "ef93ace0ba469ce840da9086827b6f77",
+    "url": "/static/media/code.ef93ace0.jpg"
+  },
+  {
+    "revision": "0714af5ef16a593a1b0f803e43b5a028",
+    "url": "/static/media/front.0714af5e.png"
+  },
+  {
+    "revision": "ea4e69800df8b33e50ebaf7c7c76a3d7",
+    "url": "/static/media/pretty.ea4e6980.jpg"
+  },
+  {
+    "revision": "bb20370cf220487208942b6581f41f32",
+    "url": "/static/media/sport.bb20370c.jpg"
+  },
+  {
+    "revision": "4543b2466806ae7ceda74f41ba41d3b1",
+    "url": "/static/media/user.4543b246.webp"
+  },
+  {
+    "revision": "cd693a491dcf44e6ccd2f54ffba693e6",
+    "url": "/static/media/work.cd693a49.jpg"
+  }
+]);
